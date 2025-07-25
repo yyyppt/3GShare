b287342609781075a18c039987defb300394be2c
yyyppt <3337402293@qq.com>
2025-07-25 23:13:35 +0800

    Initial Commit

---
+++ 3GShare()/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  3GShare()
+//
+//  Created by yyyyy on 2025/7/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 3GShare()/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  3GShare()
+//
+//  Created by yyyyy on 2025/7/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
